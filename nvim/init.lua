607b82c63ae720f616c3850b37daa63dd0bcb1b0
Ravit <ravitshrivastav@gmail.com>
2023-01-06 20:31:16 -0800

    improved tmux status bar aesthetics

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,11 @@
+if vim.g.vscode then
+    -- VSCode extension
+require("ravit.set")
+require("ravit.keymaps")
+else
+    -- ordinary Neovim
 require("ravit.set")
 require("ravit.packer")
 require("ravit.keymaps")
+end
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,4 +30,10 @@ return {
   window_background_opacity   	= 1  	, -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
   enable_tab_bar              	= false	, -- [true]
   hide_tab_bar_if_only_one_tab	= false	, -- [false] hide the tab bar when there is only a single tab in the window
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+  },
 }
