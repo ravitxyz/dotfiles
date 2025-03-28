4468cc996b2373fb10974b32aa967266dd6ae8aa
Ravit Shrivastav <ravit@Ravits-MacBook-Pro.local>
2025-03-27 20:19:31 -0700

    added mac utilities

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -2,10 +2,10 @@ return { 'alexghergh/nvim-tmux-navigation', config = function()
     require'nvim-tmux-navigation'.setup {
         disable_when_zoomed = true, -- defaults to false
         keybindings = {
+            right = "<C-l>",
             left = "<C-h>",
             down = "<C-j>",
             up = "<C-k>",
-            right = "<C-l>",
             last_active = "<C-\\>",
             next = "<C-Space>",
         }
